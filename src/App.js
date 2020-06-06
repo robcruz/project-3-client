5e7e923ed2159d199f083619c6569f3bd15e0e37
Michael Myers <anthonymyers1985@gmail.com>
2020-06-06 13:13:12 -0700

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import HomePage from './components/HomePage';
 import InfoPage from './components/InfoPage';
 import TradePage from './components/TradePage';
@@ -8,9 +7,13 @@ import NotFoundPage from './components/NotFoundPage';
 import NavBar from './NavBar';
 import './App.css';
 
+
 class App extends Component {
+
+
   render() {
     return (
+
       <Router>
         <div className='App'>
           <NavBar />
@@ -24,9 +27,11 @@ class App extends Component {
           </div>
         </div>
       </Router>
+
     );
   }
 }
+
 
 
 export default App;
